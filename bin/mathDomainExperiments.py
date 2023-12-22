36c3088dafc09e6a324aee240b0c07f6a8d691a2
Sagnik Anupam <sagnikanupam@gmail.com>
2023-12-22 03:12:58 -0500

    dataset fix

--- bin/mathDomainExperiments.py
+++ bin/mathDomainExperiments.py
@@ -15,7 +15,7 @@ from dreamcoder.utilities import numberOfCPUs
 from dreamcoder.domains.re2.main import StringFeatureExtractor
 from dreamcoder.domains.mathDomain.mathDomainPrimitives import mathDomainPrimitives
 
-TRAINING_DATASET_FILEPATHS = [Path.cwd()/'data'/'mathDomain'/'conpoleDatasetToy.csv']
+TRAINING_DATASET_FILEPATHS = [Path.cwd()/'data'/'mathDomain'/'conpoleDatasetPrefix.csv']
 
 #print(f"Training Dataset From: {TRAINING_DATASET_FILEPATHS}")
 #Check Header on Training Dataset before passing as argument
@@ -25,7 +25,7 @@ NUM_TR = training_data.shape[0] #number of training examples
 #print(f"Number of Equations Used For Training: {NUM_TR}")
 #print(training_data)
 
-TESTING_DATASET_FILEPATH = Path.cwd()/'data'/'mathDomain'/'conpoleDatasetToy.csv'
+TESTING_DATASET_FILEPATH = Path.cwd()/'data'/'mathDomain'/'conpoleDatasetPrefix.csv'
 #print(f"Testing Dataset From: {TESTING_DATASET_FILEPATH}")
 testing_data = pd.read_csv(TESTING_DATASET_FILEPATH) #pandas dataframe containing testing data
 NUM_TE = testing_data.shape[0] #number of training examples
@@ -60,7 +60,7 @@ if __name__ == "__main__":
     print("Training on: " + str(NUM_TR) + " examples.")
     print("Testing on: " + str(NUM_TE) + " examples.")
     args = commandlineArguments(
-        enumerationTimeout=600, activation='tanh',
+        enumerationTimeout=1800, activation='tanh',
         iterations=10, recognitionTimeout=3600,
         a=3, maximumFrontier=10, topK=2, pseudoCounts=30.0,
         helmholtzRatio=0.5, structurePenalty=1.,
